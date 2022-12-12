52daa715cde000618939ccde3c3f9f683e889e49
aswin <aswinr019@gmail.com>
2022-12-12 00:58:53 -0800

    test file

---
+++ test.py
@@ -0,0 +1,12 @@
+
+from Graphics import Rectangle,Circle
+from Graphics.graphics_3D import Cuboid,Sphere
+
+print(Rectangle.area_rect(3,5))
+print(Rectangle.peri_rect(3,5))
+print(Circle.area_circ(3))
+print(Circle.area_circ(3))
+print(Cuboid.peri_cuboid(1,2,3))
+print(Cuboid.area_cuboid(1,2,3))
+print(Sphere.area_sphere(3))
+print(Sphere.circumference_sphere(3))
